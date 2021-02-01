b719a5febebd84652a4ca5c546952674f50f452d
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-01 21:43:40 +0100

    #9

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { graphqlHTTP } = require("express-graphql");
 require("dotenv").config();
 
 const port = process.env.PORT || 4000;
-console.log(process.env.PORT);
+
 const app = express();
 app.use(
   "/graphql",
