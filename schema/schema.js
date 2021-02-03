a46dca0b2b694d41c65ea821435dea98b2fcba54
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-03 18:11:06 +0100

    #18

--- schema/schema.js
+++ schema/schema.js
@@ -3,12 +3,22 @@ const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 const axios = require("axios");
 require("dotenv-flow").config();
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: { type: CompanyType },
   },
 });
 
